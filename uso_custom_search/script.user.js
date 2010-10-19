0a821670a2d1a0cd90349e6fc50fa4365f2a5d4f
Tim-Smart <tim@fostle.com>
2010-10-19 20:26:59 +1300

    Added updater to uso script search

--- uso_custom_search/script.user.js
+++ uso_custom_search/script.user.js
@@ -5,6 +5,7 @@
 // @author         Tim Smart
 // @copyright      2010 (c) Tim Smart. All rights reserved.
 // @license        Creative Commons BY-NC-SA http://creativecommons.org/licenses/by-nc-sa/3.0/
+// @require        http://updater.usotools.co.cc/88396.js
 // ==/UserScript==
 
 const CONFIG = {
