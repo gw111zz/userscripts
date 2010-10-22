6d47a7c962bbe87e0ec71511178a53d12c2b9f4a
Tim-Smart <tim@fostle.com>
2010-10-23 03:31:43 +1300

    Test script creation

---
+++ test_deploy/script.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name           Test Deploy Script
+// @namespace      http://userscripts.org/users/tim
+// @include        http://*userscripts.org/*
+// @author         Tim Smart
+// @copyright      2010 (c) Tim Smart. All rights reserved.
+// @license        Creative Commons BY-NC-SA http://creativecommons.org/licenses/by-nc-sa/3.0/
+// ==/UserScript==
+
+alert('Hello world!');
+
