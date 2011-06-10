bb78633d9f425d7a45e3af3b9882d931217ee9e5
Tim <tim@fostle.com>
2011-06-10 16:16:44 +1200

    [uso_gmail] Support new v2 api

---
+++ uso_gmail/script.user.js
@@ -0,0 +1,182 @@
+// ==UserScript==
+// @name           User Scripts GMail Library
+// @description    A @require Library for UserScript's that offers a clean api to GMail.
+// @namespace      http://userscripts.org/users/tim
+// @license        MIT (See top of file)
+// @copyright      Tim Smart (c) 2011
+// ==/UserScript==
+
+// The MIT License
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+if (typeof USO !== 'object') {
+  var USO = {}
+}
+
+USO.Gmail = function (options) {
+  this.window  = unsafeWindow || window
+  this.api     = null
+  this.version = options.version || 2
+  this._events = {}
+  this.loaded  = false
+
+  var gmail = this
+
+  try {
+    if (  this.window.gmonkey
+       && 'function' === typeof this.window.gmonkey.load) {
+      var gmonkey = this.window.gmonkey
+
+      gmonkey.load(this.version, function (api) {
+        gmail.api = api
+        gmail.emit('loaded:api', api)
+      })
+    }
+  } catch (err) {
+    this.emit('error', err)
+  }
+
+  this.on('loaded:api', function () {
+    this.api.registerViewChangeCallback(function () {
+      var view = script.view
+
+      if (!view) {
+        return
+      } else if (!script.loaded) {
+        script.loaded = true
+        gmail.emit('loaded', script.api)
+      }
+
+      gmail.emit('view:change', view)
+      gmail.emit('view:' + view)
+    })
+
+    this.api.registerProfileCardCallback(function (e) {
+      gmail.emit('profilecard:change', e)
+    })
+  })
+}
+
+(function () {
+  var G = USO.Gmail
+
+  var convertToArray = function (thing) {
+    var arr = []
+
+    for (var i = 0, il = thing.length; i < il; i++) {
+      arr.push(thing[i])
+    }
+
+    return arr
+  }
+
+  G.prototype.on = function (event, cb) {
+    this._events[event] || (this._events[event] = [])
+    this._events[event].push(cb)
+
+    return this
+  }
+
+  G.prototype.removeListener = function (event, fn) {
+    if (!this._events[event]) {
+      return this
+    }
+
+    var index = this._events[event].indexOf(fn)
+
+    if (-1 === index) {
+      return this
+    }
+
+    this._events.splice(index, 1)
+
+    return this
+  }
+
+  G.prototype.emit = function () {
+    var args  = convertToArray(arguments)
+      , event = args.shift()
+
+    if (!this._events[event]) {
+      return this
+    }
+
+    for (var i = 0, il = this._events[event].length; i < il; i++) {
+      this._events[event][i].apply(this, args)
+    }
+
+    return this
+  }
+
+  G.prototype.__defineGetter__('info', function () {
+    return this.window.gmonkey.info(this.version)
+  })
+
+  G.prototype.__defineGetter__('view', function () {
+    return this.api.getActiveViewType()
+  })
+
+  G.prototype.__defineGetter__('canvas', function () {
+    return this.api.getCanvasElement()
+  })
+
+  G.prototype.__defineGetter__('active_view', function () {
+    return this.api.getActiveViewElement()
+  })
+
+  G.prototype.__defineGetter__('nav_pane', function () {
+    return this.api.getNavPaneElement()
+  })
+
+  G.prototype.__defineGetter__('masthead', function () {
+    return this.api.getMastheadElement()
+  })
+
+  G.prototype.__defineGetter__('labels', function () {
+    return this.api.getSystemLabelsElement()
+  })
+
+  G.prototype.__defineGetter__('conv_rhs', function () {
+    return this.api.getConvRhsElement()
+  })
+
+  G.prototype.__defineGetter__('footer', function () {
+    return this.api.getFooterElement()
+  })
+
+  G.prototype.addNavModule = function (title, content, color) {
+    return this.api.addNavModule(title, content, color)
+  }
+
+  G.prototype.click = function (element) {
+    var click = document.createEvent('MouseEvents')
+
+    click.initMouseEvent
+      ( 'click', true, true
+      , document.defaultView
+      , 1, 0, 0, 0, 0, 
+      , false, false, false, false
+      , 0, null
+      )
+    element.dispatchEvent(click)
+
+    return this
+  }
+})()
